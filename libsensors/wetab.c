f25f9339ddc42a10471c1943dc88a6df20e378fa
BuildTools <unconfigured@null.spigotmc.org>
2016-12-20 01:09:29 +0100

    Reverted back to hdaps

--- libsensors/wetab.c
+++ libsensors/wetab.c
@@ -1,456 +1,312 @@
-/*
- * w500_sensor.c
+/**
+ * HDAPS accelerometer sensor
  *
- *      Created on: 28.05.2012
- *      Author: Marek Vasut <marex@denx.de>
- *      Licensed under GPLv2 or later
- */
+ * Copyright (C) 2011 The Android-x86 Open Source Project
+ *
+ * by Stefan Seidel <stefans@android-x86.org>
+ * Adaptation by Tanguy Pruvot <tpruvot@github>
+ *
+ * Licensed under GPLv2 or later
+ *
+ **/
 
-#define LOG_TAG "WetabSensor"
+/* #define LOG_NDEBUG 0 */
+#define LOG_TAG "HdapsSensors"
 
-#include <linux/types.h>
-#include <linux/input.h>
-#include <linux/joystick.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <fcntl.h>
-#include <cutils/sockets.h>
-#include <cutils/log.h>
-#include <cutils/native_handle.h>
+#include <errno.h>
 #include <dirent.h>
 #include <math.h>
+#include <poll.h>
+#include <pthread.h>
+#include <linux/input.h>
+
+#include <cutils/atomic.h>
+#include <cutils/log.h>
+#include <cutils/properties.h>
 #include <hardware/sensors.h>
 
-#define DRIVER_DESC 			"Pegatron Lucid Tablet Accelerometer"
-#define SENS_COUNT			3
-#define SENS_LIGHT			0
-#define SENS_ROTATE			1
-#define SENS_ACCEL			2
-#define ID_AMBIENT_LIGHT		(SENSORS_HANDLE_BASE + SENS_LIGHT)
-#define ID_ROTATION_VECTOR		(SENSORS_HANDLE_BASE + SENS_ROTATE)
-#define ID_ACCELEROMETER		(SENSORS_HANDLE_BASE + SENS_ACCEL)
+#define INPUT_DIR		"/dev/input"
+#define ARRAY_SIZE(a)		(sizeof(a) / sizeof(a[0]))
+#define ID_ACCELERATION		(SENSORS_HANDLE_BASE + 0)
 
-#define VALID_LIGHT			(1 << 0)
-#define VALID_ROTATE			(1 << 1)
-#define VALID_ACCEL			(1 << 2)
+#define AMIN(a,b)		(((a)<(fabs(b)))?(a):(b))
+#define SQUARE(x)		((x)*(x))
+#define COS_ASIN(m,x)		(sqrt(SQUARE(m)-SQUARE(AMIN(m,x))))
+#define COS_ASIN_2D(m,x,y)	(COS_ASIN(m,x)*COS_ASIN(m,y)/(m))
 
-#define VALID_ROTATE_X			(1 << 0)
-#define VALID_ROTATE_Y			(1 << 1)
-#define VALID_ROTATE_Z			(1 << 2)
-#define VALID_ROTATE_MASK		(7 << 0)
+typedef struct {
+	const char *name;
+	float conv[3];
+	int swap[3];
+	int avg_cnt;
+} accel_params;
 
-struct sensor_context {
-	struct sensors_poll_device_t device;
-	int accel_fd;
+/* precision, result data should give GRAVITY_EARTH ~9.8 */
+#define CONVERT			(GRAVITY_EARTH / 156.0f)
+#define CONVERT_PEGA		(GRAVITY_EARTH / 256.0f)
+#define CONVERT_LIS		(GRAVITY_EARTH / 1024.0f)
 
-	struct timespec delay;
+/* axis swap for tablet pcs, X=0, Y=1, Z=2 */
+#define NO_SWAP			{ 0, 1, 2 }
+#define SWAP_YXZ		{ 1, 0, 2 }
+#define SWAP_ZXY		{ 2, 0, 1 }
 
-	uint32_t sent;
-	uint32_t valid;
-
-	int light_data;
-	int orientation_data[3];
-	uint8_t orientation_valid;
+static accel_params accel_list[] = {
+	{ "hdaps", { CONVERT, -CONVERT, 0 }, NO_SWAP, 1 },
+	{ "Pegatron Lucid Tablet Accelerometer", { CONVERT_PEGA, CONVERT_PEGA, CONVERT_PEGA }, NO_SWAP, 4 },
+//	{ "ST LIS3LV02DL Accelerometer",  { -CONVERT_LIS, CONVERT_LIS, CONVERT_LIS }, SWAP_YXZ, 2 }, /* tablet mode */
+	{ "ST LIS3LV02DL Accelerometer",  { CONVERT_LIS, -CONVERT_LIS, CONVERT_LIS }, SWAP_ZXY, 2 }, /* pc mode */
 };
 
-static int open_accel_sensor(void)
-{
-	const char *dirname = "/dev/input";
-	DIR *dir;
-	struct dirent *de;
-	char name[PATH_MAX];
-	int ret;
+static accel_params accelerometer;
+static sensors_vec_t *events_q = NULL;
+static int event_cnt = 0;
+static unsigned int forced_delay = 0;
+
+struct sensors_poll_context_t {
+	struct sensors_poll_device_t device;
 	int fd;
+};
 
-	fd = -1;
-	dir = opendir(dirname);
-	if (dir != NULL) {
-		/*
-		 * Loop over all "jsXX" in /dev/input and look
-		 * for our driver.
-		 */
-		ALOGD("%s[%i] Looping over all jsXX...", __func__, __LINE__);
-		while ((de = readdir(dir))) {
-			if (de->d_name[0] != 'j')
-				continue;
-			memset(name, 0, PATH_MAX);
-			snprintf(name, PATH_MAX, "%s/%s", dirname, de->d_name);
-			ALOGD("%s[%i] Open device %s", __func__, __LINE__, name);
+static int common__close(struct hw_device_t *dev) {
+	struct sensors_poll_context_t *ctx = (struct sensors_poll_context_t *) dev;
+	free(ctx);
+	free(events_q);
+	events_q = NULL;
 
-			fd = open(name, O_RDWR);
-			if (fd < 0) {
-				ALOGD("%s[%i] Could not open %s, %s", __func__,
-					__LINE__, name, strerror(errno));
-				continue;
-			}
+	return 0;
+}
 
-			name[PATH_MAX - 1] = '\0';
-			ret = ioctl(fd, JSIOCGNAME(PATH_MAX - 1), &name);
-			if (ret < 1) {
-				ALOGD("%s[%i] Could not get device name for "
-					"%s, %s\n", __func__, __LINE__,
-					name, strerror(errno));
-				name[0] = '\0';
-			}
+static int device__activate(struct sensors_poll_device_t *dev, int handle,
+		int enabled) {
 
-			ALOGI("%s[%i] Testing device %s",
-					__func__, __LINE__, name);
+	return 0;
+}
 
-			if (!strcmp(name, DRIVER_DESC)) {
-				ALOGI("%s[%i] Found device %s",
-					__func__, __LINE__, name);
+static int device__set_delay(struct sensors_poll_device_t *device, int handle,
+		int64_t ns) {
+	forced_delay = ns / 1000;
+	return 0;
+
+}
+
+static int device__poll(struct sensors_poll_device_t *device,
+		sensors_event_t *data, int count) {
+
+	struct input_event event;
+	struct sensors_poll_context_t *dev =
+			(struct sensors_poll_context_t *) device;
+
+	accel_params signs;
+	char prop[PROPERTY_VALUE_MAX] = "";
+	float val;
+	int x, y, z;
+
+	if (dev->fd < 0)
+		return 0;
+
+	// dynamic axis tuning, expect "1,-1,-1" format
+	if (property_get("hal.sensors.axis.revert", prop, 0)) {
+		sscanf(prop, "%d,%d,%d", &x, &y, &z);
+		ALOGD("axis signs set to %d %d %d", x, y, z);
+	} else {
+		x = y = z = 1;
+	}
+	signs.conv[ABS_X] = accelerometer.conv[ABS_X] * x;
+	signs.conv[ABS_Y] = accelerometer.conv[ABS_Y] * y;
+	signs.conv[ABS_Z] = accelerometer.conv[ABS_Z] * z;
+
+	ALOGV("axis convert set to %.6f %.6f %.6f",
+		signs.conv[ABS_X], signs.conv[ABS_Y] ,signs.conv[ABS_Z]);
+
+	// dynamic axis swap, expect "0,1,2" format
+	if (property_get("hal.sensors.axis.order", prop, 0)) {
+		sscanf(prop, "%d,%d,%d", &x, &y, &z);
+		ALOGD("axis order set to %c %c %c", 'x'+x, 'x'+y, 'x'+z);
+	} else {
+		// use default values (accel_params)
+		x = accelerometer.swap[0];
+		y = accelerometer.swap[1];
+		z = accelerometer.swap[2];
+	}
+
+	while (read(dev->fd, &event, sizeof(event)) > 0) {
+
+		ALOGV("gsensor event %d - %d - %d", event.type, event.code, event.value);
+
+		if (event.type == EV_ABS) {
+			switch (event.code) {
+			// Even though this mapping results in wrong results with some apps,
+			// it just means that these apps are broken, i.e. they rely on the
+			// fact that phone have portrait displays. Laptops/tablets however
+			// have landscape displays, and the axes are relative to the default
+			// screen orientation, not relative to portrait orientation.
+			// See the nVidia Tegra accelerometer docs if you want to know for sure.
+			case ABS_X: // 0x00
+			case ABS_Y: // 0x01
+			case ABS_Z: // 0x02
+				val = signs.conv[event.code] * event.value;
+				if (event.code == ABS_X)
+					events_q[event_cnt].v[x] = val;
+				else if (event.code == ABS_Y)
+					events_q[event_cnt].v[y] = val;
+				else if (event.code == ABS_Z)
+					events_q[event_cnt].v[z] = val;
 				break;
 			}
+		} else if (event.type == EV_SYN) {
+			int i;
+			data->timestamp = (int64_t) ((int64_t) event.time.tv_sec
+					* 1000000000 + (int64_t) event.time.tv_usec * 1000);
+			// hdaps doesn't have z-axis, so simulate it by rotation matrix solution
+			if (signs.conv[2] == 0)
+				events_q[event_cnt].z = COS_ASIN_2D(GRAVITY_EARTH, events_q[event_cnt].x, events_q[event_cnt].y);
+			memset(&data->acceleration, 0, sizeof(sensors_vec_t));
+			for (i = 0; i < accelerometer.avg_cnt; ++i) {
+				data->acceleration.x += events_q[i].x;
+				data->acceleration.y += events_q[i].y;
+				data->acceleration.z += events_q[i].z;
+			}
+			data->sensor = ID_ACCELERATION;
+			data->type = SENSOR_TYPE_ACCELEROMETER;
+			data->acceleration.status = SENSOR_STATUS_ACCURACY_HIGH;
+			if (++event_cnt >= accelerometer.avg_cnt)
+				event_cnt = 0;
 
-			close(fd);
+			// spare the CPU if desired
+			if (forced_delay)
+				usleep(forced_delay);
+			return 1;
+		}
+	}
+
+	return -errno;
+}
+
+static int open_input_device(void) {
+	char *filename;
+	int fd = -1;
+	DIR *dir;
+	struct dirent *de;
+	char name[80];
+	char devname[256];
+	dir = opendir(INPUT_DIR);
+	if (dir == NULL)
+		return -1;
+
+	strcpy(devname, INPUT_DIR);
+	filename = devname + strlen(devname);
+	*filename++ = '/';
+
+	while ((de = readdir(dir))) {
+		int f;
+		size_t i;
+		if (de->d_name[0] == '.')
+			continue;
+		strcpy(filename, de->d_name);
+		f = open(devname, O_RDONLY);
+		if (f < 0) {
+			continue;
 		}
 
-		ALOGD("%s[%i] stop loop and closing directory",
-			__func__, __LINE__);
-		closedir(dir);
+		if (ioctl(f, EVIOCGNAME(sizeof(name) - 1), &name) < 1) {
+			name[0] = '\0';
+		}
+
+		ALOGV("%s name is %s", devname, name);
+
+		for (i = 0; i < ARRAY_SIZE(accel_list); ++i) {
+			if (!strcmp(name, accel_list[i].name)) {
+				int c = accel_list[i].avg_cnt;
+				memcpy(&accelerometer, &accel_list[i], sizeof(accel_params));
+				accelerometer.conv[0] = accel_list[i].conv[0] / c;
+				accelerometer.conv[1] = accel_list[i].conv[1] / c;
+				accelerometer.conv[2] = accel_list[i].conv[2] / c;
+				events_q = calloc(c, sizeof(sensors_vec_t));
+				ALOGI("found %s at %s", name, devname);
+				break;
+			}
+		}
+		if (events_q) {
+			fd = f;
+			break;
+		}
+		close(f);
 	}
+	closedir(dir);
 
 	return fd;
 }
 
-static int context__activate(struct sensors_poll_device_t *dev,
-				int handle, int enabled)
-{
-	struct sensor_context* ctx = (struct sensor_context *)dev;
-	int fd;
-
-	switch (handle) {
-	case SENS_ROTATE:
-		if (ctx->accel_fd >= 0)
-			return 0;
-
-		fd = open_accel_sensor();
-		if (fd < 0)
-			return -EINVAL;
-
-		ctx->accel_fd = fd;
-		return 0;
-
-	case SENS_LIGHT:
-	case SENS_ACCEL:
-		/* Light and fake accel need no activation. */
-		return 0;
-	}
-
-	return -EINVAL;
-}
-
-static int context__setDelay(struct sensors_poll_device_t *dev,
-				int handle, int64_t ns)
-{
-	struct sensor_context* ctx = (struct sensor_context *)dev;
-
-	ctx->delay.tv_sec = 0;
-	ctx->delay.tv_nsec = ns;
-
-	return 0;
-}
-
-static int context__close(struct hw_device_t *dev)
-{
-	struct sensor_context* ctx = (struct sensor_context *)dev;
-
-	close(ctx->accel_fd);
-
-	free(ctx);
-
-	return 0;
-}
-
-static int poll_data(struct sensors_poll_device_t *dev, sensors_event_t *data)
-{
-	struct input_event iev;
-	struct sensor_context *ctx = (struct sensor_context *)dev;
-	size_t res;
-	float val;
-	int i;
-
-	/* Orientation sensor */
-	for (;;) {
-		res = read(ctx->accel_fd, &iev, sizeof(struct input_event));
-		if (res != sizeof(struct input_event))
-			break;
-
-		if (iev.type != EV_ABS)
-			continue;
-
-		switch (iev.code) {
-		case ABS_X:
-			ctx->orientation_data[0] = iev.value;
-			ctx->orientation_valid |= VALID_ROTATE_X;
-			break;
-		case ABS_Y:
-			ctx->orientation_data[1] = iev.value;
-			ctx->orientation_valid |= VALID_ROTATE_Y;
-			break;
-		case ABS_Z:
-			ctx->orientation_data[2] = iev.value;
-			ctx->orientation_valid |= VALID_ROTATE_Z;
-			break;
-		case ABS_MISC:
-			ctx->light_data = iev.value;
-			ctx->valid |= VALID_LIGHT;
-			return 0;
-		default:
-			break;
-		}
-
-		if (ctx->orientation_valid == VALID_ROTATE_MASK) {
-			ctx->valid |= VALID_ROTATE;
-			return 0;
-		}
-	}
-
-	return 0;
-}
-
-static int craft_fake_accel(struct sensors_poll_device_t *dev, sensors_event_t *data)
-{
-	struct sensor_context *ctx = (struct sensor_context *)dev;
-
-	const double angle = 20.0;
-	const double cos_angle = GRAVITY_EARTH * cos(angle / M_PI);
-	const double sin_angle = GRAVITY_EARTH * sin(angle / M_PI);
-
-	/*
-	 *              y=max
-	 *                |
-	 *                |
-	 *                |
-	 *                |
-	 * x=max ---------+--------- x=min
-	 *                |
-	 *                |
-	 *                |
-	 *                |
-	 *              y=min
-	 */
-
-	if (ctx->orientation_data[0] >= 10000) {
-		/* Landscape */
-		data->acceleration.x = 0.0f;
-		data->acceleration.y = cos_angle;
-		data->acceleration.z = sin_angle;
-	} else if (ctx->orientation_data[0] <= -10000) {
-		/* Flipped landscape */
-		data->acceleration.x = 0.0f;
-		data->acceleration.y = -cos_angle;
-		data->acceleration.z = -sin_angle;
-	} else if (ctx->orientation_data[1] <= -10000) {
-		/* Portrait */
-		data->acceleration.x = cos_angle;
-		data->acceleration.y = 0.0f;
-		data->acceleration.z = sin_angle;
-	} else if (ctx->orientation_data[1] >= 10000) {
-		/* Flipped portrait */
-		data->acceleration.x = -cos_angle;
-		data->acceleration.y = 0.0f;
-		data->acceleration.z = -sin_angle;
-	} else {
-		/* No change */
-		return 0;
-	}
-
-	return 1;
-}
-
-
-static int submit_sensor(struct sensors_poll_device_t *dev, sensors_event_t *data)
-{
-	struct sensor_context *ctx = (struct sensor_context *)dev;
-	int ret = 0;
-	struct timespec t;
-	const uint32_t end = ctx->sent;
-	uint32_t start = end;
-	ctx->sent = (ctx->sent + 1) % SENS_COUNT;
-
-	/* Get time for the event. */
-	memset(&t, 0, sizeof(t));
-	clock_gettime(CLOCK_MONOTONIC, &t);
-	data->timestamp = ((int64_t)(t.tv_sec) * 1000000000LL) + t.tv_nsec;
-	data->version = sizeof(*data);
-
-	do {
-		start = (start + 1) % SENS_COUNT;
-
-		switch (ctx->valid & (1 << start)) {
-		case VALID_LIGHT:
-			data->sensor = ID_AMBIENT_LIGHT;
-			data->type = SENSOR_TYPE_LIGHT;
-
-			data->light = ctx->light_data;
-
-			ALOGD("%s[%i] LIGHT %d", __func__, __LINE__,
-					ctx->light_data);
-
-			ctx->valid &= ~(1 << start);
-			return 1;
-		case VALID_ROTATE:
-			data->sensor = ID_ROTATION_VECTOR;
-			data->type = SENSOR_TYPE_ROTATION_VECTOR;
-			data->orientation.status = SENSOR_STATUS_ACCURACY_MEDIUM;
-
-			float x = ctx->orientation_data[0];
-			float y = ctx->orientation_data[1];
-			float z = ctx->orientation_data[2];
-
-			data->orientation.x = x;
-			data->orientation.y = y;
-			data->orientation.z = z;
-
-			ALOGD("%s[%i] ROTATE %d %d %d -> %f %f %f",
-				__func__, __LINE__,
-				ctx->orientation_data[0],
-				ctx->orientation_data[1],
-				ctx->orientation_data[2],
-				data->orientation.x,
-				data->orientation.y,
-				data->orientation.z);
-
-			ctx->valid &= ~(1 << start);
-			ctx->valid |= VALID_ACCEL;
-			return 3;
-		case VALID_ACCEL:
-			data->sensor = ID_ACCELEROMETER;
-			data->type = SENSOR_TYPE_ACCELEROMETER;
-			data->acceleration.status = SENSOR_STATUS_ACCURACY_HIGH;
-
-			ret = craft_fake_accel(dev, data);
-
-			ALOGD("%s[%i] ACCEL %f %f %f",
-				__func__, __LINE__,
-				data->acceleration.x,
-				data->acceleration.y,
-				data->acceleration.z);
-
-			ctx->valid &= ~(1 << start);
-			return ret;
-		}
-	} while (start != end);
-
-	return 0;
-}
-
-static int context__poll(struct sensors_poll_device_t *dev, sensors_event_t *data, int count)
-{
-	struct sensor_context *ctx = (struct sensor_context *)dev;
-	int ret;
-
-	while (1) {
-
-		poll_data(dev, data);
-
-		ret = submit_sensor(dev, data);
-		if (ret)
-			return ret;
-
-		nanosleep(&ctx->delay, &ctx->delay);
-	}
-
-	return 0;
-}
-
-static const struct sensor_t sensor_list[] = {
-	[0] = {
-		.name		= "W500 Ambient Light sensor",
-		.vendor		= "Unknown",
-		.version	= 1,
-		.handle		= ID_AMBIENT_LIGHT,
-		.type		= SENSOR_TYPE_LIGHT,
-		.maxRange	= 100,
-		.resolution	= 1,
-		.power		= 1,
-		.minDelay	= 100,
-	},
-	[1] = {
-		.name		= "W500 Gyro sensor",
-		.vendor		= "BOSCH Sensortec",
-		.version	= 1,
-		.handle		= ID_ROTATION_VECTOR,
-		.type		= SENSOR_TYPE_ROTATION_VECTOR,
-		.maxRange	= 35000.0f,
-		.resolution	= 1.0f,
-		.power		= 1,
-		.minDelay	= 100,
-	},
-	[2] = {
-		.name		= "W500 Fake accelerometer",
-		.vendor		= "FooBar Technology Group",
-		.version	= 1,
-		.handle		= ID_ACCELEROMETER,
-		.type		= SENSOR_TYPE_ACCELEROMETER,
-		.maxRange	= 2.8f,
-		.resolution	= 1.0f/4032.0f,
-		.power		= 3.0f,
-		.minDelay	= 100,
+static const struct sensor_t sSensorList[] = {
+	{	.name = "HDAPS accelerometer",
+		.vendor = "Linux kernel",
+		.version = 1,
+		.handle = ID_ACCELERATION,
+		.type = SENSOR_TYPE_ACCELEROMETER,
+		.maxRange = (GRAVITY_EARTH * 6.0f),
+		.resolution = (GRAVITY_EARTH * 6.0f) / 1024.0f,
+		.power = 0.84f,
+		.reserved = {},
 	},
 };
 
-static int sensors__get_sensors_list(struct sensors_module_t *module,
-					const struct sensor_t **list)
-{
-	*list = sensor_list;
-	return sizeof(sensor_list)/sizeof(sensor_list[0]);
-}
+static int open_sensors(const struct hw_module_t* module, const char* name,
+		struct hw_device_t** device);
 
+static int sensors__get_sensors_list(struct sensors_module_t* module,
+		struct sensor_t const** list) {
+	*list = sSensorList;
 
-static int open_sensors(const struct hw_module_t *module, const char* id,
-			struct hw_device_t **device)
-{
-	struct sensor_context *ctx;
-	int fd;
-
-	if (strcmp(id, SENSORS_HARDWARE_POLL))
-		return -EINVAL;
-
-	/* Allocate context */
-	ctx = malloc(sizeof(*ctx));
-	if (!ctx)
-		return -ENOMEM;
-
-	memset(ctx, 0, sizeof(*ctx));
-
-	ctx->accel_fd = -1;
-	ctx->delay.tv_sec = 0;
-	ctx->delay.tv_nsec = 100;
-
-	/* Do common setup */
-	ctx->device.common.tag = HARDWARE_DEVICE_TAG;
-	ctx->device.common.version = 0;
-	ctx->device.common.module = (struct hw_module_t *)module;
-	ctx->device.common.close = context__close;
-
-	ctx->device.activate = context__activate;
-	ctx->device.setDelay = context__setDelay;
-	ctx->device.poll = context__poll;
-
-	*device = &ctx->device.common;
-
-	return 0;
-
-err_light:
-	close(ctx->accel_fd);
-err_accel:
-	free(ctx);
-	return -ENOMEM;
+	return ARRAY_SIZE(sSensorList);
 }
 
 static struct hw_module_methods_t sensors_module_methods = {
-	.open	= open_sensors
+	.open = open_sensors
 };
 
 struct sensors_module_t HAL_MODULE_INFO_SYM = {
 	.common = {
-		.tag		= HARDWARE_MODULE_TAG,
-		.version_major	= 1,
-		.version_minor	= 0,
-		.id		= SENSORS_HARDWARE_MODULE_ID,
-		.name		= "W500 SENSORS Module",
-		.author		= "Marek Vasut",
-		.methods	= &sensors_module_methods,
+		.tag = HARDWARE_MODULE_TAG,
+		.version_major = 2,
+		.version_minor = 0,
+		.id = SENSORS_HARDWARE_MODULE_ID,
+		.name = "hdaps accelerometer sensor",
+		.author = "Stefan Seidel",
+		.methods = &sensors_module_methods,
+		.dso = NULL,
+		.reserved = {},
 	},
-	.get_sensors_list	= sensors__get_sensors_list,
+	.get_sensors_list = sensors__get_sensors_list
 };
+
+static int open_sensors(const struct hw_module_t* module, const char* name,
+		struct hw_device_t** device) {
+	int status = -EINVAL;
+
+	struct sensors_poll_context_t *dev = calloc(1,
+			sizeof(struct sensors_poll_context_t));
+
+	dev->device.common.tag = HARDWARE_DEVICE_TAG;
+	dev->device.common.version = 0;
+	dev->device.common.module = (struct hw_module_t*) module;
+	dev->device.common.close = common__close;
+	dev->device.activate = device__activate;
+	dev->device.setDelay = device__set_delay;
+	dev->device.poll = device__poll;
+
+	if ((dev->fd = open_input_device()) < 0) {
+		ALOGE("GSensor get class path error");
+	} else {
+		*device = &dev->device.common;
+		status = 0;
+	}
+
+	return status;
+}
